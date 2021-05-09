6685eadd7ea7b9b5b850efd2feb476c1eb56692c
KaiRuLin <10746041@ntub.edu.tw>
2021-05-09 20:57:13 +0800

    modify Login.vue's login icon

--- plearnet-vue/src/components/Login.vue
+++ plearnet-vue/src/components/Login.vue
@@ -1,19 +1,31 @@
 <template>
-    <router-link to="/user/self"><button class="btn">登入</button></router-link>
-    <!-- <Test text="Add Task" color="red" /> -->
+
+  <router-link to="/user/self"><img v-bind:src="lop" alt="" id="lop" /></router-link>
+  <!-- <router-link to="/user/self"><button class="btn">登入</button></router-link> -->
+  <!-- <Test text="Add Task" color="red" /> -->
 </template>
 
 <script>
 // import Test from './Test'
 
 export default {
-    name: 'Login',
-    porops: {
-        text: String,
-        color: String,
-    },
-    components:{
-        // Test,
-    }
-}
+  name: "Login",
+  data() {
+    return {
+      lop: require("../../static/img/lop.png"),     
+    };
+  },
+  porops: {
+    text: String,
+    color: String,
+  },
+  components: {
+    // Test,
+  },
+};
 </script>
+<style scoped>
+#lop{
+    width:70px;
+}
+</style>
