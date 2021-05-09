f2144e695269215c446ad8d5646afca4f6f5e140
KaiRuLin <10746041@ntub.edu.tw>
2021-05-09 20:23:50 +0800

    Created Search.vue and modify Header.vue

--- plearnet-vue/src/components/Header.vue
+++ plearnet-vue/src/components/Header.vue
@@ -1,67 +1,10 @@
 <template>
   <header>
     <div class="search-box">
-    <router-link to="/"><img v-bind:src="img" alt="" id="home" class="home"/></router-link>
-
-      <div class="search-control">
-         <input class="search" :placeholder="prompt + selected" />
-      <div class="search-select">
-        <button @click="show = !show" class="search-class">▼</button>
-        <transition
-          @enter="enter"
-          @leave="leave"
-          @after-enter="afterEnter"
-          @after-leave="afterLeave"
-        >
-          <div v-if="show">
-            <div class="block" id="v-model-radiobutton">
-              <div>
-                <input
-                  type="radio"
-                  value="標題"
-                  v-model="selected"
-                  id="title"
-                  name="selected"
-                />
-                <label for="title">標題</label>
-              </div>
-
-              <div>
-                <input
-                  type="radio"
-                  value="作者"
-                  id="user"
-                  v-model="selected"
-                  name="selected"
-                />
-                <label for="user">作者</label>
-              </div>
-              <div>
-                <input
-                  type="radio"
-                  value="日期"
-                  id="date"
-                  v-model="selected"
-                  name="selected"
-                />
-                <label for="date">日期</label>
-              </div>
-              <div>
-                <input
-                  type="radio"
-                  value="分類"
-                  id="class"
-                  v-model="selected"
-                  name="selected"
-                />
-                <label for="class">分類</label>
-              </div>
-            </div>
-          </div>
-        </transition>
-      </div>
-      </div>
-     
+      <router-link to="/"
+        ><img v-bind:src="img" alt="" id="home" class="home"
+      /></router-link>
+      <Search />
     </div>
     <div class="button-box">
       <Login />
@@ -71,7 +14,7 @@
 
 <script>
 import Login from "./Login";
-
+import Search from "./Search"
 export default {
   name: "Header",
   data() {
@@ -85,6 +28,7 @@ export default {
 
   components: {
     Login,
+    Search
   },
 };
 </script>
@@ -96,12 +40,12 @@ header {
   align-items: center;
   padding: 3px 200px 3px 200px;
 }
-.search-box{
-  display: flex;  
+.search-box {
+  display: flex;
   align-items: center;
 }
 
-.search-box>.search-control {
+.search-box > .search-control {
   display: flex;
   width: 200px;
   height: 25px;
@@ -121,50 +65,8 @@ header {
   align-items: center;
   justify-content: center;
 }
-#home {
+.home {
   width: 50px;
-}
-.home{
   margin: 3px 20px 0px 0px;
-}
-.v-leave {
-  opacity: 1;
-}
-
-.v-leave-active {
-  transition: opacity 0.5s;
-}
-
-.v-leave-to {
-  opacity: 0;
-}
-
-.v-enter {
-  opacity: 0;
-}
-
-.v-enter-active {
-  transition: opacity 0.5s;
-}
-
-.v-enter-to {
-  opacity: 1;
-}
-
-button {
-  margin-bottom: 10px;
-}
-.block {
-  display: flex;
-  background: rgb(255, 255, 255);
-  width: 230px;
-  height: 30px;
-  margin-top: 10px;
-  border-radius: 5px;
-  position: absolute;
-  left: 265px;
-}
-.search-class {
-  border-radius: 100px;
 }
 </style>
