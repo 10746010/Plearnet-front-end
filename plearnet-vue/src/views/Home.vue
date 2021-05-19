3e85b64f2a01de5d005f30ac203ecefad6d60d00
KaiRuLin <10746041@ntub.edu.tw>
2021-05-19 18:17:00 +0800

    Update Login.vue, Register.vue, Self.vue

---
+++ plearnet-vue/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <Top />
+    <Classification />
+  </div>
+</template>
+
+<script>
+import Top from '../components/Carousel'
+import Classification from '../components/Classification'
+
+export default {
+  name: 'Home',
+  components:{
+    Top,
+    Classification,
+  }
+}
+</script>
+<style scoped>
+
+</style>
